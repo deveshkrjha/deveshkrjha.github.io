364651dd0879567f9368570bfa54b056c80adcf1
Devesh <debu1.jha@gmail.com>
2025-06-17 10:44:48 +0530

    updated

--- My Projects/Project 1/script.js
+++ My Projects/Project 1/script.js
@@ -1,6 +1,8 @@
 const loginForm = () => {
     const str = `<div>
     <h3>Login Form</h3>
+    <input type="email" placeholder="Enter your email"/>
+    <input type="password" placeholder="Enter your password"/>
     <p><button onclick='showHome()'>Submit</button></p>
     <p><button onclick='registerForm()'>Create Account</button></p>
     `
@@ -10,6 +12,9 @@ const loginForm = () => {
 const registerForm = () => {
      const str = `<div>
     <h3>Registration Form</h3>
+    <input type="text" placeholder="Enter your name"/>
+    <input type="email" placeholder="Enter your email"/>
+    <input type="password" placeholder="Enter your password"/>
     <p><button onclick='loginForm()'>Submit</button></p>
     <p><button onclick='loginForm()'>Already a member? Login here...</button></p>
     `
