9fd07d35553383ea27c28d88f0b5a6e86c298864
Devesh <debu1.jha@gmail.com>
2025-06-18 09:33:41 +0530

    update

--- My Projects/Project 1/script.js
+++ My Projects/Project 1/script.js
@@ -1,30 +1,83 @@
+let users = [];
+let user = {};
+
+const validateUser = () => {
+  let email = document.getElementById("txtEmail").value;
+  let pass = document.getElementById("txtPass").value;
+  const found = users.find(
+    (value) => value.email === email && value.pass === pass
+  );
+  if (found) {
+    showHome();
+  } else {
+    document.getElementById("errorTxt").innerHTML = "Access Denied";
+  }
+};
+
+const renderUserList = () => {
+    let x= "<h4>Registered Users:</h4>";
+    if(users.length==0){
+        return "<p>No Users Registered</p>";
+
+    }
+    users.forEach(user => {
+        x += `<p>${user.name} | ${user.email} | ${user.pass} | ${user.balance}</p>`;
+    });
+    
+    return x;
+};
+
 const loginForm = () => {
-    const str = `<div>
+  const str = `
+    <div style='display:flex'>
+    <div>
     <h3>Login Form</h3>
-    <input type="email" placeholder="Enter your email"/>
-    <input type="password" placeholder="Enter your password"/>
-    <p><button onclick='showHome()'>Submit</button></p>
+    <p id='errorTxt'></p>
+    <p><input type='text' id='txtEmail'></p>
+    <p><input type='password' id='txtPass'></p>
+    <p><button onclick='validateUser()'>Submit</button></p>
     <p><button onclick='registerForm()'>Create Account</button></p>
-    `
-    root.innerHTML = str + "</div>"
-}
+    </div>
+    <div>
+     ${renderUserList()}
+    </div>
+    </div>
+    `;
+  root.innerHTML = str;
+};
+
+const saveUser = () => {
+  let name = document.getElementById("txtName").value;
+  let email = document.getElementById("txtEmail").value;
+  let pass = document.getElementById("txtPass").value;
+  users.push({
+    name,
+    email,
+    pass,
+    balance:1000,
+  });
+  loginForm();
+};
 
 const registerForm = () => {
-     const str = `<div>
+  const str = `<div>
     <h3>Registration Form</h3>
-    <input type="text" placeholder="Enter your name"/>
-    <input type="email" placeholder="Enter your email"/>
-    <input type="password" placeholder="Enter your password"/>
-    <p><button onclick='loginForm()'>Submit</button></p>
+    <p><input type='text' id='txtName'></p>
+    <p><input type='text' id='txtEmail'></p>
+    <p><input type='password' id='txtPass'></p>
+    <p><button onclick='saveUser()'>Submit</button></p>
     <p><button onclick='loginForm()'>Already a member? Login here...</button></p>
-    `
-    root.innerHTML = str + "</div>"
-}
+    `;
+  root.innerHTML = str + "</div>";
+};
 
 const showHome = () => {
-     const str = `<div>
+  const str = `<div>
     <h3>Welcome</h3>
     <p><button onclick='loginForm()'>Logout</button></p>
-    `
-    root.innerHTML = str + "</div>"
-}
+    `;
+  root.innerHTML = str + "</div>";
+};
+
+
+loginForm()
