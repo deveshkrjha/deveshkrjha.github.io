277e4258fff797b61d7497e6bfd635b43c60eb2a
Devesh <debu1.jha@gmail.com>
2025-06-20 10:00:24 +0530

    UPDate

---
+++ JAVASCRIPT/file1.js
@@ -0,0 +1,25 @@
+//
+let student="John"
+let age=21
+let promoted=true
+let score
+let city=null
+
+//console.log(student)
+// console.log(typeof student)
+// console.log(typeof age)
+ 
+//TYPECASTING
+/*console.log(typeof age)
+let strAge=String(age)
+console.log(typeof  strage)
+
+let rating="5"
+console.log(typeof rating);
+let strRating=Number(rating);
+console.log(typeof strRating);
+
+let flag="true";
+let bFlag=Boolean(flag);
+console.log(typeof bFlag);*/
+
---
+++ JAVASCRIPT/file2.js
@@ -0,0 +1,36 @@
+//OPERATORS IN JAVASCRIPT
+// let a=54;
+// let b=10;
+// console.log(a+b); 
+// console.log(a%b);
+
+// let a="10";
+// let b=10;
+// console.log(a+b);
+// console.log(a*b);//because of concanitation javascript automatically takes string as number during multiplication
+
+// let a=2;
+// let b=2;
+// console.log(a==b);
+
+// let a="2";
+// let b=2;
+// console.log(a==b);
+// console.log(a===b);
+
+// let a=10;
+// let b=2;
+// let c=12;
+// console.log(a>b && a>c);//LOGICAL OPERATORS
+
+// let a=10;
+// let b=2;
+// let c=12;
+// console.log(a>b || a>c);//LOGICAL OPERATORS
+
+let a=10;
+let b=2;
+// console.log(!a>b);
+console.log(a**b);//A TO THE POWER B
+
+
